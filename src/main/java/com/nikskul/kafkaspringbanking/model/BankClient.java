27598f6c8d8d6d47149bbee2329d48a002f8b1ea
nikskul <nikitaskul2012@gmail.com>
2022-07-01 20:27:19 +0300

    Remove ID (username is uniq). Add JSON props

--- src/main/java/com/nikskul/kafkaspringbanking/model/BankClient.java
+++ src/main/java/com/nikskul/kafkaspringbanking/model/BankClient.java
@@ -1,19 +1,36 @@
 package com.nikskul.kafkaspringbanking.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.util.UUID;
 
 @Data
 public class BankClient {
 
-    private final UUID id;
-
+    @JsonProperty("username")
     private final String username;
+    @JsonProperty("password")
     private final String password;
 
+    @JsonProperty("firstName")
     private final String firstName;
+    @JsonProperty("middleName")
     private final String middleName;
+    @JsonProperty("lastName")
     private final String lastName;
 
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public BankClient(
+            @JsonProperty("username") String username,
+            @JsonProperty("password") String password,
+            @JsonProperty("firstName") String firstName,
+            @JsonProperty("middleName") String middleName,
+            @JsonProperty("lastName") String lastName
+    ) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
 }
