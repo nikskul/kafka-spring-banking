4e440ce457db53744ae5236de0f83531d2e7e680
nikskul <nikitaskul2012@gmail.com>
2022-07-01 20:16:39 +0300

    Refactor

--- src/main/java/com/nikskul/kafkaspringbanking/controller/WithdrawalController.java
+++ src/main/java/com/nikskul/kafkaspringbanking/controller/WithdrawalController.java
@@ -3,8 +3,6 @@ package com.nikskul.kafkaspringbanking.controller;
 import com.nikskul.kafkaspringbanking.request.OperationRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
